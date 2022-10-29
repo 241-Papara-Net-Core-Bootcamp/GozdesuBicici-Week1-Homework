19390f67e90a88155e5cc5b684b19e73299861dc
Gözdesu Biçici <gzds.bcc@gmail.com>
2022-10-29 15:19:53 +0300

    Add project files.

---
+++ Owner.API/Controllers/OwnerController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Owner.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Owner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+
+        private List<OwnerModel> owners = new List<OwnerModel>();
+        OwnerModel owner1 = new OwnerModel
+        {
+            ID = 1,
+            FirstName = "Gözdesu",
+            LastName = "Biçici",
+            Date = DateTime.Now,
+            Description = "Papara .Net Core Eğitim Öğrencisi",
+            Type = "Mezun",
+        };
+
+        OwnerModel owner2 = new OwnerModel
+        {
+            ID = 2,
+            FirstName = "Damla",
+            LastName = "Çopur",
+            Date = DateTime.Now,
+            Description = "Tarih",
+            Type = "Mezun",
+        };
+
+        OwnerModel owner3 = new OwnerModel
+        {
+            ID = 3,
+            FirstName = "Yiğit",
+            LastName = "Biçici",
+            Date = DateTime.Now,
+            Description = "Mühendis",
+            Type = "Mezun",
+        };
+
+
+
+        [Route("Details")]
+        [HttpGet]
+        public List<OwnerModel> GetOwnerDetails()
+        {
+            owners.Add(owner1);
+            owners.Add(owner2);
+            owners.Add(owner3);
+
+            return owners;
+        }
+
+        [HttpPost]
+        public OwnerModel Post([FromBody] OwnerModel owner)
+        {
+          
+                owners.Add(owner);
+                return owner;
+            
+
+        }
+
+        [HttpPut]
+        public OwnerModel EditOwner([FromBody] OwnerModel owner)
+        {
+            var editedOwner = owners.FirstOrDefault(x => x.ID == owner.ID);
+            editedOwner.FirstName = owner.FirstName;
+            editedOwner.LastName = owner.LastName;
+            editedOwner.Date = owner.Date;
+            editedOwner.Description = owner.Description;
+            editedOwner.Type = owner.Type;
+
+            return owner;
+        }
+
+        [HttpDelete("id")]
+        public void Delete(int id)
+        {
+            var deletedOwner = owners.FirstOrDefault(x => x.ID == id);
+            owners.Remove(deletedOwner);
+        }
+    }
+}
---
+++ Owner.API/Models/OwnerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Owner.API.Models
+{
+    public class OwnerModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
